b147ee9ab88136dbf62c27b5fd16788931291a68
rubenbuelvas <rcbuelvasv@gmail.com>
2021-03-08 22:56:18 -0500

    solve_grid_values included in Sudoku class

--- sudoku_hill.py
+++ sudoku_hill.py
@@ -20,7 +20,9 @@ unit_types = ['row', 'column', 'unit3x3']  # unit_type can be 'row', 'column', o
 print_display = 'nothing'  # Values: ["nothing", "minimum", "init grid", "init and final grids", "all solution grids"]
 
 """
-from colorama import Fore, Back, Style  # example: print(Fore.BLUE + displaystring)
+import random
+import time
+from colorama import Back, Style  # example: print(Fore.BLUE + displaystring)
 
 
 class Sudoku:
@@ -28,6 +30,7 @@ class Sudoku:
     def __init__(self, grid, showif=0.0):
         self.showif = showif
         self.cols = '123456789'
+        self.digits = '123456789'
         self.empty_digits = '0. '
         self.rows = 'ABCDEFGHI'
         self.unit_types = ['row', 'column', 'unit3x3']
@@ -53,6 +56,8 @@ class Sudoku:
         self.gv_conflicts = None
         self.total_conflicts = 0
         self.first_squares_of_unit3x3 = self.cross('ADG', '147')
+        self.search_methods = {'Brute Force', 'Norvig Heuristic', 'Norvig Improved', 'Hill Climbing'}
+        self.search_method = 'Hill Climbing'
 
     def cross(self, A, B):
         # Cross product of elements in A and elements in B.
@@ -122,7 +127,7 @@ class Sudoku:
 
     def initial_squares_set(self):
         # Will receive the initial grid and return a set of all squares not empty
-        # loop trough all squares and list unempty squares (initial puzzle)
+        # loop trough all squares and list non-empty squares (initial puzzle)
         set_initial_squares = set()
         for r in self.rows:
             for c in self.cols:
@@ -144,7 +149,7 @@ class Sudoku:
         # Reminder:   units['C2'] == [['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'], #column
         #                             ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'], #line
         #                             ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']] #unit 3x3
-        new_grid_values = self.gv_init.copy()  # dictionnary of initial values. Ex: {'A1': '4', 'A2': '.', 'A3': '.', ...
+        new_grid_values = self.gv_init.copy()  # dict of initial values. Ex: {'A1': '4', 'A2': '.', 'A3': '.', ...
 
         # Go through all 3x3 units and fill empty squares with random unused digits
         for fsou in self.first_squares_of_unit3x3:  # loop through the 9 units.
@@ -162,19 +167,21 @@ class Sudoku:
 
             # Fill empty squares randomly
             remaining_digits = list(
-                shuffled(self.cols.translate({ord(c): '' for c in digitsused}))  # removes digits + shuffle
+                shuffled(self.digits.translate({ord(c): '' for c in digitsused}))  # removes digits + shuffle
             )
             for s in listofsquareswithoutvalue:
                 self.gv_current[s] = remaining_digits.pop()
             if len(remaining_digits) != 0:
-                raise ValueError(f"Programming error: remaining digits should be empty but contains: {remaining_digits}")
+                raise ValueError(
+                    f"Programming error: remaining digits should be empty but contains: {remaining_digits}")
 
-    def count_conflicts(self):
+    def count_conflicts(self, grid):
         """Receives the initial grid and the current grid and returns a total evaluation of the
            conflicts in the grid (sum of all conflicts)"""
-        tmp, self.total_conflicts, tmp = self.eval_conflicts()
+        tmp, self.total_conflicts, tmp = self.eval_conflicts(grid)
+        return self.total_conflicts
 
-    def eval_conflicts(self):
+    def eval_conflicts(self, grid):
         """Receives the initial grid and the current grid and returns:
            - # A grid_values of conflicts (dict of {square: nb_of_conflits}
            - A total evaluation of the conflicts in the grid (sum of all conflicts)
@@ -192,25 +199,26 @@ class Sudoku:
         # Will loop through each square
         for r in self.rows:
             for c in self.cols:
-                conflictvalue = 0
-                if (self.gv_init[r + c] not in self.empty_digits) or (self.gv_current[r + c] in self.emptydigits):
-                    conflictvalue = 0  # square filled in the initial puzzle or current grid with empty digits
+                conflict_value = 0
+                if (self.gv_init[r + c] not in self.empty_digits) or (self.grid[r + c] in self.empty_digits):
+                    conflict_value = 0  # square filled in the initial puzzle or current grid with empty digits
                 else:  # square randomly filled in the current grid
                     # Will calculate the number of conflict
-                    u_column, u_line = self.units[r + c][0], self.units[r + c][1]  # conflicts in column and conflicts in line
+                    # Conflicts in column and conflicts in line
+                    u_column, u_line = self.units[r + c][0], self.units[r + c][1]
                     for s in u_column:
-                        if (s != r + c) and self.gv_current[s] == self.gv_current[r + c]: conflictvalue += 1
+                        if (s != r + c) and self.grid[s] == self.grid[r + c]:
+                            conflict_value += 1
                     for s in u_line:
-                        if (s != r + c) and self.gv_current[s] == self.gv_current[r + c]: conflictvalue += 1
+                        if (s != r + c) and self.grid[s] == self.grid[r + c]:
+                            conflict_value += 1
 
                 # Update dictionaries
-                conflicts_grid_values[r + c] = conflictvalue  # Put the value (int) of the conflict in grid_value
-                if not conflicts_dict.get(conflictvalue):
-                    conflicts_dict[conflictvalue] = []  # List of squares (values) for this number of conflicts (key)
-                conflicts_dict[conflictvalue].append(r + c)
-
-                conflictvaluestotal += conflictvalue
-
+                conflicts_grid_values[r + c] = conflict_value  # Put the value (int) of the conflict in grid_value
+                if not conflicts_dict.get(conflict_value):
+                    conflicts_dict[conflict_value] = []  # List of squares (values) for this number of conflicts (key)
+                conflicts_dict[conflict_value].append(r + c)
+                conflictvaluestotal += conflict_value
         return conflicts_grid_values, conflictvaluestotal, conflicts_dict
 
     # Display as 2-D grid
@@ -249,98 +257,101 @@ class Sudoku:
             if r in 'CF': displaystring = displaystring + line + ('    ' + line if show_conflicts else '') + '\n'
         print(displaystring)
 
+    # def swap2numbers(gv_init, gv_current, conflicts_grid_values):
+    #     """Will take a square with a maximum number of conflicts and swap it randomly within the 3x3 unit with
+    #        another square that isn't part of the initial puzzle. Will then return the new grid_value"""
+    #
+    #     # Selects randomly one of the squares with the maximum number of conflicts
+    #     s_withmaxconflicts, maxconflicts = squares_causing_max_conflicts(gv_init, gv_current, conflicts_grid_values)
+    #     s1toswap = some(shuffled(s_withmaxconflicts)) # random selection
+    #
+    #     # Find another square within 3x3 unit and swap randomly without considering the number of conflicts (TO IMPROVE?)
+    #     s2toswap = some(shuffled(possible_replacements_within_unit(gv_init, s1toswap, 'unit3x3')))
+    #     s1value = gv_current[s1toswap]
+    #     gv_current[s1toswap] = gv_current[s2toswap]
+    #     gv_current[s2toswap] = s1value
+    #
+    #     if print_display != "nothing":
+    #         print(f"Swapped {s1toswap }={gv_current[s1toswap]} and {s2toswap}={gv_current[s2toswap]} (values after the swap)")
+    #
+    #     return gv_current
 
-# def swap2numbers(gv_init, gv_current, conflicts_grid_values):
-#     """Will take a square with a maximum number of conflicts and swap it randomly within the 3x3 unit with
-#        another square that isn't part of the initial puzzle. Will then return the new grid_value"""
-#
-#     # Selects randomly one of the squares with the maximum number of conflicts
-#     s_withmaxconflicts, maxconflicts = squares_causing_max_conflicts(gv_init, gv_current, conflicts_grid_values)
-#     s1toswap = some(shuffled(s_withmaxconflicts)) # random selection
-#
-#     # Find another square within 3x3 unit and swap randomly without considering the number of conflicts (TO IMPROVE?)
-#     s2toswap = some(shuffled(possible_replacements_within_unit(gv_init, s1toswap, 'unit3x3')))
-#     s1value = gv_current[s1toswap]
-#     gv_current[s1toswap] = gv_current[s2toswap]
-#     gv_current[s2toswap] = s1value
-#
-#     if print_display != "nothing":
-#         print(f"Swapped {s1toswap }={gv_current[s1toswap]} and {s2toswap}={gv_current[s2toswap]} (values after the swap)")
-#
-#     return gv_current
+    # Search
+    def solve_grid_values(self, print_display=False):
+        # Will solve a puzzle with the appropriate search method
+        self.fill_grid_randomly()  # Fills all the 3x3 units randomly with unused numbers in unit
 
+        if print_display in ['init grid', 'init and final grids', 'all solution grids']:
+            self.display_gv(True)
 
+        if self.search_method == 'Hill Climbing':
+            self.gv_current = self.improve_solution_hill_climb_calc_all_swaps3x3()
+            if print_display in ["init and final grids", "all solution grids"]: self.display_gv()
+        else:
+            raise ValueError(
+                f"Unknown search method {self.search_method}. Available search methods are {self.search_methods}")
 
+    def improve_solution_hill_climb_calc_all_swaps3x3(self):
+        """Receives a puzzle with conflicts and tries to decrease the number of conflicts by swapping 2 values
+           using the Hill Climbing method.
+           Will calculate total conflict for all possible swaps of a pair within a 3x3 unit and then choose the best"""
 
+        # Create a list of swappable pairs (tuple) for each unit (not part of initial square)
+        all_swappables_squares = self.non_initial_squares_set()  # Will only consider non initial squares as swappable
+        set_of_swappable_pairs = set()  # Example: {('A3', 'B1'), ('A3', 'B3'), ('A3', 'C3')...
+        for r in self.rows:
+            for c in self.cols:
+                if r + c in all_swappables_squares:
+                    # Will loop for all combinations within units
+                    possible_swaps = set(
+                        self.squares_within_unit_list(r + c, 'unit3x3')
+                    ) - {r + c}  # squares in unit, except current
+                    possible_swaps = possible_swaps.intersection(all_swappables_squares)  # only swappable squares
+                    for s in possible_swaps:
+                        # Will insert the pairs in a set, and the first element of the pair will always be the smallest.
+                        if r + c < s:
+                            set_of_swappable_pairs.add((r + c, s))
+                        else:
+                            set_of_swappable_pairs.add((s, r + c))
 
+        # Will simulate each swap, calculate the total conflicts for each swap and choose the best
+        best_puzzle = self.gv_current
+        current_total_conflicts = self.count_conflicts()
+        best_total_conflicts = current_total_conflicts
 
+        while True:  # Loop until a maximum is found
+            for pair in set_of_swappable_pairs:
+                test_puzzle = self.gv_current.copy()
+                # swap the 2 values
+                test_puzzle[pair[0]], test_puzzle[pair[1]] = self.gv_current[pair[1]], self.gv_current[pair[0]]
+                if self.count_conflicts(test_puzzle) < best_total_conflicts:  # found a better candidate
+                    best_total_conflicts = self.count_conflicts(test_puzzle)
+                    best_puzzle = test_puzzle
 
-
-################ Search ################
-def solve_grid_values(grid, searchMethod, printdisplay=False):
-    """ Will solve a puzzle with the appropriate search method"""
-    # Initializes a puzzle
-    gv_init = grid_values(grid)
-    gv_current = fillgridrandomly(gv_init)  # Fills all the 3x3 units randomly with unused numbers in unit
-
-    if print_display in ["init grid", "init and final grids", "all solution grids"]:
-        displaygridv(gv_init, gv_current, True)
-
-    if searchMethod == 'Hill Climbing':
-        gv_new = improve_solution_hill_climb_calc_all_swaps3x3(gv_init, gv_current)
-        if print_display in ["init and final grids", "all solution grids"]: displaygridv(gv_init, gv_new)
-    else:
-        raise ValueError(f"Unknown search method {searchMethod}. Available search methods are {searchMethods}")
-    return gv_init, gv_new
-
-
-def improve_solution_hill_climb_calc_all_swaps3x3(gv_init, gv_current):
-    """Receives a puzzle with conflicts and tries to decrease the number of conflicts by swapping 2 values
-       using the Hill Climbing method.
-       Will calculate total conflict for all possible swaps of a pair within a 3x3 unit and then choose the best"""
-
-    # Create a list of swappable pairs (tuple) for each unit (not part of initial square)
-    all_swappables_squares = non_initial_squares_set(gv_init)  # Will only consider non initial squares as swappable
-    set_of_swappable_pairs = set()  # Example: {('A3', 'B1'), ('A3', 'B3'), ('A3', 'C3')...
-    for r in rows:
-        for c in cols:
-            if r + c in all_swappables_squares:
-                # Will loop for all combinations within units
-                possible_swaps = set(squares_within_unit_list(r + c, "unit3x3")) - {
-                    r + c}  # squares in unit, except current
-                possible_swaps = possible_swaps.intersection(all_swappables_squares)  # only swappable squares
-                for s in possible_swaps:
-                    # Will insert the pairs in a set, and the first element of the pair will always be the smallest.
-                    if r + c < s:
-                        set_of_swappable_pairs.add((r + c, s))
-                    else:
-                        set_of_swappable_pairs.add((s, r + c))
-
-    # Will simulate each swap, calculate the total conflicts for each swap and choose the best
-    best_puzzle = gv_current
-    current_total_conflicts = countconflicts(gv_init, gv_current)
-    best_total_conflicts = current_total_conflicts
-
-    while True:  # Loop until a maximum is found
-        for pair in set_of_swappable_pairs:
-            test_puzzle = gv_current.copy()
-            test_puzzle[pair[0]], test_puzzle[pair[1]] = gv_current[pair[1]], gv_current[pair[0]]  # swap the 2 values
-            if countconflicts(gv_init, test_puzzle) < best_total_conflicts:  # found a better candidate
-                best_total_conflicts = countconflicts(gv_init, test_puzzle)
-                best_puzzle = test_puzzle
-
-        if best_total_conflicts == current_total_conflicts:  # no improvement (local maximum or solution)
-            if print_display != 'nothing': print(
-                f"FINAL SOLUTION: found maximum with {best_total_conflicts} conflicts.")
-            return gv_current
-        else:  # will try to improve
-            if print_display != 'nothing': print(f"Swapping{pair} and total conflicts is now {best_total_conflicts}")
-            if print_display == 'all solution grids': displaygridv(gv_init, best_puzzle)
-            gv_current = best_puzzle
-            current_total_conflicts = best_total_conflicts
+            if best_total_conflicts == current_total_conflicts:  # no improvement (local maximum or solution)
+                if print_display != 'nothing':
+                    print(f'FINAL SOLUTION: found maximum with {best_total_conflicts} conflicts.')
+                return self.gv_current
+            else:  # will try to improve
+                self.gv_current = best_puzzle
+                current_total_conflicts = best_total_conflicts
+                if print_display != 'nothing':
+                    print(f'Swapping{pair} and total conflicts is now {best_total_conflicts}')
+                if print_display == 'all solution grids':
+                    self.display_gv()
 
 
-################ Utilities ################
+    # System test
+
+    def solved(self, values):
+        "A puzzle is solved if each unit is a permutation of the digits 1 to 9."
+
+        def unit_solved(unit):
+            return set(values[s] for s in unit) == set(self.digits)
+
+        return values is not False and all(unit_solved(unit) for unit in self.unit_list)
+
+# Utilities
 def some(seq):
     "Return some element of seq that is true."
     for e in seq:
@@ -360,8 +371,7 @@ def shuffled(seq):
     return seq
 
 
-################ System test ################
-import time, random
+# System test
 
 
 def solve_all(grids, name='', showif=0.0, searchMethod='ToSpecify'):
@@ -406,12 +416,6 @@ def solve_all(grids, name='', showif=0.0, searchMethod='ToSpecify'):
             sum(results), N, name, sum(times), sum(times) / N, hz, max(times), searchMethod))
 
 
-def solved(values):
-    "A puzzle is solved if each unit is a permutation of the digits 1 to 9."
-
-    def unitsolved(unit): return set(values[s] for s in unit) == set(digits)
-
-    return values is not False and all(unitsolved(unit) for unit in unitlist)
 
 
 ################ Main routine ################
